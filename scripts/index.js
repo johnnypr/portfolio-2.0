18134cdd7e2d1e9bf93ce5ce1fc3967946e65ea4
johnnypr <johnnypr11@icloud.com>
2024-10-29 08:15:52 -0400

    js setInteval change

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,12 @@ var image = document.getElementById("dog-walk");
 var change = 0;
 
 
-setInterval(function(){
-        image.style.left = change+"px";
-        change-=1.8;
-        if(change>200) {
-            clearInterval();
-        }
+function dogMove() {
+    image.style.left = change+"px";
+    change-=1.8;
+    if(change>200) {
+        clearInterval();
     }
-    ,50);
+}
+
+setInterval(dogMove,50);
